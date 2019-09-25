1379049f7fcdba23f1c75867ecae724808df5c91
1483158687 <1483158687@qq.com>
2019-09-25 15:02:43 +0800

    Initial commit

---
+++ hahaha/src/firstjavacode/chapter1/hellowrold.java
@@ -0,0 +1,10 @@
+package firstjavacode.chapter1;
+
+/**
+ * Created by sysadmin on 9/25/19.
+ */
+public class hellowrold {
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
+}
