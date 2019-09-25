e908dcbbcc5f45b7302c266d368f8a6499cdb1a5
1483158687 <1483158687@qq.com>
2019-09-25 15:32:57 +0800

    hh

---
+++ hahaha/src/firstjavacode/chapter2/People.java
@@ -0,0 +1,13 @@
+package firstjavacode.chapter2;
+
+/**
+ * Created by sysadmin on 9/25/19.
+ */
+public class People {
+    int name;
+    int age;
+    public void print(){
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
